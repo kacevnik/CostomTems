da4e32a99f61a1e4db1c5ce47067092a0e87d155
kacevnik <kacevnik@yandex.ru>
2017-10-30 13:15:43 +0300

    Добавление плагина Unyson из репазитория

--- tgm/custom_theme.php
+++ tgm/custom_theme.php
@@ -62,14 +62,9 @@ function kovalev_register_required_plugins() {
 		// This is an example of how to include a plugin bundled with a theme.
 		array(
 			'name'               => 'Unyson plugin', // The plugin name.
-			'slug'               => 'unyson-plugin', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/lib/plugins/unyson.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+			'slug'               => 'Unyson', // The plugin slug (typically the folder name).
+			'required'  		 => true,
+			'force_activation'   => true,
 		),
 
 		// This is an example of how to include a plugin from the WordPress Plugin Repository.
