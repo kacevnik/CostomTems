a98bf48644f62b708d1393b8eac060b09ef8f76e
kacevnik <kacevnik@yandex.ru>
2017-11-03 10:01:45 +0300

    Плавная прокрутка

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function ($) {		
-	$('.section_3').css({'background-position-x': ($(window).width() - 1200)/2 -365 + 'px'});
 
-	$('.footer_menu ul').css({'margin-left': ($('.footer_menu').width()-$('.footer_menu ul').width())/2+'px'});
+	//Плавная прокрутка
+  	$("a[href*='#']").bind("click", function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 500);
+		e.preventDefault();
+		return false;
+	});
 });
